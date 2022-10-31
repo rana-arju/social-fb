61cb1c387e09bf08439ad86822430f0322e119eb
rana-arju <arjurana20@gmail.com>
2022-10-31 17:55:12 +0530

    main header design

--- src/components/home/header/Header.js
+++ src/components/home/header/Header.js
@@ -1,43 +1,68 @@
 import React from "react";
 import "./style.css";
 import {Link} from "react-router-dom";
-import {Friends, Gaming, HomeActive, Logo, Market, Search, Watch} from "../../../svg"
+import {useSelector} from "react-redux"
+import {Friends, Gaming, HomeActive, Logo, Market, Menu, Messenger, Notifications, Search, Watch} from "../../../svg";
 const Header = () => {
+    const {user} = useSelector((user) => ({...user}))
+    console.log("user", user);
     const color = "#65676b"
   return (
     <header>
       <div className="header_left">
         <Link to="/" className="header-logo">
-            <div className="circle">
-                <Logo />
-            </div>
+          <div className="circle">
+            <Logo />
+          </div>
         </Link>
         <div className="search search1">
-            <Search color={color} />
-            <input type="text" name="" value="" placeholder="Search Facebook" className="hide_input" />
+          <Search color={color} />
+          <input
+            type="text"
+            name=""
+            value=""
+            placeholder="Search Facebook"
+            className="hide_input"
+            onChange={() => ""}
+          />
         </div>
       </div>
       <div className="header_middle">
         <Link to="/" className="middle_icon active">
-            <HomeActive color={color} />
+          <HomeActive color={color} />
         </Link>
         <Link to="/" className="middle_icon hover1">
-            <Friends color={color} />
+          <Friends color={color} />
         </Link>
         <Link to="/" className="middle_icon hover1">
-            <Watch color={color} />
-            <div className="middle_notification">
-                10+
-            </div>
+          <Watch color={color} />
+          <div className="middle_notification">10+</div>
         </Link>
         <Link to="/" className="middle_icon hover1">
-            <Market color={color} />
+          <Market color={color} />
         </Link>
         <Link to="/" className="middle_icon hover1">
-            <Gaming color={color} />
+          <Gaming color={color} />
         </Link>
       </div>
-      <div className="header_right"></div>
+      <div className="header_right">
+        <div className="circle_icon">
+          <Menu />
+        </div>
+        <div className="circle_icon">
+          <Messenger />
+        </div>
+        <div className="circle_icon">
+          <Notifications />
+          <div className="right_notification">
+            12
+          </div>
+        </div>
+        <Link to="/profile" className="profile_link hover1">
+          <img src={user?.picture} alt="" />
+         
+        </Link>
+      </div>
     </header>
   );
 };
