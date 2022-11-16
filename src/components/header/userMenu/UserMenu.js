790c8361b62c6d7e11baf29ce2f15fc5b327ac43
rana-arju <arjurana20@gmail.com>
2022-11-16 20:43:37 +0530

    logout functionality add

--- src/components/header/userMenu/UserMenu.js
+++ src/components/header/userMenu/UserMenu.js
@@ -1,10 +1,21 @@
+import Cookies from "js-cookie";
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
 import DisplayAccessibility from "./DisplayAccessibility";
 import HelpSupport from "./HelpSupport";
 import SettingsPrivacy from "./SettingsPrivacy";
 const UserMenu = ({ user }) => {
-    const [visible, setVisible] = useState(0)
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [visible, setVisible] = useState(0);
+  const logout = () => {
+    Cookies.set("user", "");
+    dispatch({
+      type: "LOGOUT",
+    });
+    navigate("/login");
+  };
   return (
     <div className="mmenu">
       {visible === 0 && (
@@ -56,7 +67,7 @@ const UserMenu = ({ user }) => {
               <i className="right_icon"></i>
             </div>
           </div>
-          <div className="mmenu_item hover3">
+          <div className="mmenu_item hover3" onClick={() => logout()}>
             <div className="small_circle">
               <i className="logout_filled_icon"></i>
             </div>
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,30 +1,32 @@
-import React, { useRef, useState } from 'react';
-import { useSelector } from 'react-redux';
-import CreatePost from '../../components/createPost';
-import Header from '../../components/header/Header';
-import LeftMenu from '../../components/home/leftMenu';
-import RightMenu from '../../components/home/right';
-import Stories from '../../components/home/stories';
-import useClickOutside from '../../helpers/ClickOutside';
+import React, { useRef, useState } from "react";
+import { useSelector } from "react-redux";
+import CreatePost from "../../components/createPost";
+import Header from "../../components/header/Header";
+import LeftMenu from "../../components/home/leftMenu";
+import RightMenu from "../../components/home/right";
+import EmailVerificatin from "../../components/home/sendVerification";
+import Stories from "../../components/home/stories";
+import useClickOutside from "../../helpers/ClickOutside";
 import "./style.css";
 const Home = () => {
-    const [visible, setVisible] = useState(true);
-    const {user} = useSelector((state) => ({...state}))
-    const el = useRef(null);
-    useClickOutside(el, () => {
-        setVisible(false)
-    });
-    return (
-      <div className="home">
-        <Header />
-        <LeftMenu user={user} />
-        <div className="home_midlle">
-          <Stories user={user} />
-          <CreatePost user={user} />
-        </div>
-        <RightMenu user={user} />
+  const [visible, setVisible] = useState(true);
+  const { user } = useSelector((state) => ({ ...state }));
+  const el = useRef(null);
+  useClickOutside(el, () => {
+    setVisible(false);
+  });
+  return (
+    <div className="home">
+      <Header />
+      <LeftMenu user={user} />
+      <div className="home_midlle">
+        <Stories user={user} />
+        {user.verified === false && <EmailVerificatin user={user} />}
+        <CreatePost user={user} />
       </div>
-    );
+      <RightMenu user={user} />
+    </div>
+  );
 };
 
 export default Home;
